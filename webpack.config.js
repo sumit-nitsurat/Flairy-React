ccfbe3abc93107faad51008dd6e5012f632c76fe
sumitnitsurat <sumit.nitsurat@gmail.com>
2017-01-05 21:17:14 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devServer: {
         inline: true,
         contentBase: './src',
-        port: 3002,
+        port: 3000,
         historyApiFallback: true
     },
     devtool: 'cheap-module-eval-source-map',
