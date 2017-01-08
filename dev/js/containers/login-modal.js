11c7f8cbb01012ca9e1a216349c9b756426dd655
sumit-nitsurat <sumit.nitsurat@gmail.com>
2017-01-08 20:18:43 +0530

    Added facebook login and google plus login functionality

--- dev/js/containers/login-modal.js
+++ dev/js/containers/login-modal.js
@@ -1,20 +1,18 @@
 import React, {Component} from 'react';
 import classNames from 'classnames';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 class LoginModal extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
     let cName = classNames({
         'login-btn': this.props.isFullSection
     });
-    let btnStyle = {
-        marginLeft: '18%',
-        marginTop: '15px',
-        width: '70%'
-    },loginHeading = {
+    let loginHeading = {
         fontFamily: "fantasy",
         fontSize: "24px",
         marginTop: "20px",
@@ -27,14 +25,51 @@ class LoginModal extends Component {
                     <h4 className="modal-title">One step away to explore the world of opportunities !!!</h4>
                 </div>
                 <div className={cName}>
-                    <button className="btn btn-lg btn-primary btn-block" style={btnStyle}>
-                        <i className="icon-facebook" style={{padding:"8px"}}></i>Login with Facebook</button>
-                    <button className="btn btn-lg btn-danger btn-block" style={btnStyle}>
-                        <i className="icon-mail" style={{padding:"8px"}}></i>Login with Gmail</button>                
+                    {this.facebookLogin()} 
+                    {this.googleLogin()}               
                 </div>
       </div>
     )
   }
+  responseFacebook(response) {
+    console.log(response);
+  };
+
+  facebookLogin() {
+      return (
+           <FacebookLogin
+                appId="xxxx"
+                autoLoad={false}
+                fields="name,email,picture"
+                icon="fa-facebook fb-icon"
+                cssClass="loginBtn"
+                callback={this.responseFacebook.bind(this)}/>
+      );
+  }
+  
+    responseGoogle (response){
+        console.log(response);
+    }
+ 
+    googleLogin() {
+        let btnStyle ={
+           background: '#9d261d'
+        }
+        return (
+        <span>
+        <GoogleLogin
+            clientId="xxxx"
+            buttonText="LOGIN WITH GMAIL"
+            onSuccess={this.responseGoogle.bind(this)}
+            onFailure={this.responseGoogle.bind(this)}
+            offline={false}
+            style={btnStyle}
+            className='loginBtn'>
+             <span>
+                        <i className="icon-mail" style={{padding:"8px"}}></i>LOGIN WITH GMAIL</span>  
+        </GoogleLogin>
+        </span>);
+    }
 }
 
 export default LoginModal;
